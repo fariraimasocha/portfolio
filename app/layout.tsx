a1ec17edad569bb5c357b96ea24c68fd3b02aa3a
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-10-13 10:05:41 +0200

    wip
    update metadata logic for image preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,29 @@ const playfair = Playfair_Display({
 })
 
 export const metadata: Metadata = {
-  title: 'fariraimasocha',
+  title: {
+    default: 'fariraimasocha',
+    template: '%s | fariraimasocha'
+  },
   description: 'A portfolio showcasing some of my work',
+  metadataBase: new URL('https://portfolio-78f0jlqla-farirais-projects.vercel.app/'),
   openGraph: {
-    images: '/images/meta/preview.png'
+    title: 'fariraimasocha',
+    description: 'A portfolio showcasing some of my work',
+    images: [
+      {
+        url: '/images/meta/preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'fariraimasocha portfolio preview'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'fariraimasocha',
+    description: 'A portfolio showcasing some of my work',
+    images: ['/images/meta/preview.png'],
   }
 }
 
