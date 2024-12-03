af8113efce2aba5ffb2fc5e7b3c10b16618adf05
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-12-03 11:13:53 +0200

    fix
    show user toast on notification

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -9,7 +9,6 @@ import { ContactFormSchema } from '@/lib/schemas'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { sendEmail } from '@/lib/actions'
 
 type Inputs = z.infer<typeof ContactFormSchema>
 
@@ -29,13 +28,7 @@ export default function ContactForm() {
     })
 
     const processForm: SubmitHandler<Inputs> = async data => {
-        const result = await sendEmail(data)
-
-        if (result?.error) {
-            toast.error('An error occurred! Please try again.')
-            return
-        }
-
+        // For now, just show success toast without sending data
         toast.success('Message sent successfully!')
         reset()
     }
